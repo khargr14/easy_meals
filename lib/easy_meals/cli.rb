a50538afd8524db27c25eb497f6792a17343dd69
Keesha Hargrove <kharg14@gmail.com>
2020-08-18 11:50:00 +0000

    update

--- lib/easy_meals/cli.rb
+++ lib/easy_meals/cli.rb
@@ -2,15 +2,24 @@ class EasyMeals::CLI
   
   def call
     puts "Welcome Easy Fun Fast Meals"
+    categories_list
     menus
+    
   end
 
-def menus
+def categories_list
    puts <<-DOC
    1. Holiday Sides Made With 5 Ingredients Or Less
    2.Three Five-Ingredient Appetizers For Last-Minute Holiday Parties
    3. 4 easy 3-Ingredient Vegetable Side Dishes
  DOC
 end
+
+def menu
+  puts "Enter the number of the "
+  
+end
+
+
 
 end
