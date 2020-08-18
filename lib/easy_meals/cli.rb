e27e8f1916f85260e4a0ee3ac2b22660fce82591
Keesha Hargrove <kharg14@gmail.com>
2020-08-18 23:28:17 +0000

    update doc

--- lib/easy_meals/cli.rb
+++ lib/easy_meals/cli.rb
@@ -3,18 +3,18 @@ class EasyMeals::CLI
   def call
     puts "Welcome !!!
     Easy Fun Fast Meals"
-    #categories_list
+    categories_list
     #menus
     
   end
 
 def categories_list
    puts <<-DOC
-   Hello its all Tasty Recipes CLI! In this CLI app, you will be able to search for recipes using a keyword such as 'chicken', then see a list of recipes on Tasty.co  website. You can then request more details on the recipe! Lets make easy fun meals!
+   Hello its all Tasty Recipes CLI! In this CLI app, you will be able to search for recipes using a keyword such as 'chicken', 'beef' or whatever your heart desires**, then see a list of recipes on Tasty.co  website. You can then request more details on the recipe! Lets make easy fun meals!
    _______________________________________________________________
    
-   1. Holiday Sides Made With 5 Ingredients Or Less
-   2.Three Five-Ingredient Appetizers For Last-Minute Holiday Parties
+   1. 5 Ingredients Or Less
+   2. Three Five-Ingredient Appetizers For Last-Minute 
    3. 4 easy 3-Ingredient Vegetable Side Dishes
  DOC
 end
