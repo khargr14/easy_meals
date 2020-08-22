978a88a1b347cbb84e23f20e89f7148b01c80167
Keesha Hargrove <kharg14@gmail.com>
2020-08-22 00:58:25 +0000

    update

--- lib/easy_meals/recipe.rb
+++ lib/easy_meals/recipe.rb
@@ -1,5 +1,17 @@
 class EasyMeals::Recipe
-  attr_accessor :name, :total_time, :url, :ingredients, :directions
+  attr_accessor :name, :category , :url, :ingredients, :directions #:total_time
+ 
+ def initialize(name, url, category)
+    @name = name
+    @url = url
+    @category = category
+    @ingredients = []
+    @instructions = []
+    self.class.all << self
+  end
+
+ 
+ 
   
  def self.all 
    #i should return a bunch of instance of Recipe
